da57ae381c4d75493c4d6a8b180422e77233b41c
pyPlacca <dhegreatprosperity10@gmail.com>
2020-09-18 13:01:37 +0000

    update server

--- main.rb
+++ main.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
 
 get '/' do
-	'</h1>Simple Ruby Server</h1>'
+	"
+	<h1>
+		Welcome to the world of
+		<a href='https://ruby-lang.org'>Ruby</a>
+	</h1>
+	<p><a href='/about'>About</a></p>
+	"
+end
+
+get '/about' do
+	"
+		<p>This web server was built with <em>sinatra</em></p>
+		<a href='/'>&#xab; Back home</a>
+	"
+end
